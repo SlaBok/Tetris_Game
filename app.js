a4ed074955e324fce2eb447333e2d4c998c2d57e
SlaBok <bokszczaninslawomir@gmail.com>
2023-06-05 22:25:30 +0200

    Second commit

--- app.js
+++ app.js
@@ -3,9 +3,89 @@ document.addEventListener('DOMContentLoaded', () => {
     let squeres = Array.from(document.querySelectorAll('.grid div'))
     const ScoreDisplay = document.querySelector('#score')
     const Startbtn = document.querySelector('#start-button')
-    const widt = 10
+    const width = 10
 
     //console.log(squeres)
 
-})
+//Tetrominoes
+const lTetromino = [
+    [1, width+1, width*2+1, 2],
+    [width, width+1, width+2, width*2+2],
+    [1, width+1,width*2+1, width*2],
+    [width, width*2, width*2+1, width*2+2]
+]
 
+const zTetromino = [
+    [0, width, width+1, width*2+1],
+    [width+1, width+2, width*2, width*2+1],
+    [0, width, width+1, width*2+1],
+    [width+1, width+2, width*2, width*2+1]
+]
+
+const tTetromino = [
+    [1,width,width+1,width+2],
+    [1,width+1,width+2,width*2+1],
+    [width,width+1,width+2,width*2+1],
+    [1,width,width+1,width*2+1]
+]
+
+const oTetromino = [
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1]
+]
+
+const iTetromino = [
+    [1, width+1, width*2+1, width*3+1],
+    [width,width+1,width+2,width+3],
+    [1, width+1, width*2+1, width*3+1],
+    [width,width+1,width+2,width+3]
+]
+
+const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
+
+let currentPosition = 4
+let currentRotation = 0
+
+let random = Math.floor(Math.random()*theTetrominoes.length)
+let current = theTetrominoes[random][currentRotation]
+
+function draw() {
+    current.forEach(index => {
+        squares[currentPosition + index].classList.add('tetromino')
+    })
+}
+
+function undraw() {
+    current.forEach(index => {
+        squares[currentPosition + index].classList.add('tetromino')
+    })
+}
+
+
+// make the tetromino move down every second
+timerId = setInterval(moveDown, 1000)
+
+//move down function
+function moveDown() {
+    undraw()
+    currentPosition += width
+    draw()
+    freeze()
+}
+
+//frezze function
+function freeze() {
+if(current.some(index => squeres[currentPosition + index + width].classList.List.contains('taken'))){
+    current.forEach(index => squeres[currentPosition + index].classList.add('taken'))
+    random = math.flo(Math.random() * theTetrominoes.length)
+    current = theTetrominoes[random][currentRotation]
+    currentPosition = 4
+    draw()
+    }
+}
+
+
+}
+)
