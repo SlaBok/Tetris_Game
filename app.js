f35c842d45dff60586ea3b4093d851b5331a7a66
SlaBok <bokszczaninslawomir@gmail.com>
2023-06-05 19:31:10 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid')
+    let squeres = Array.from(document.querySelectorAll('.grid div'))
+    const ScoreDisplay = document.querySelector('#score')
+    const Startbtn = document.querySelector('#start-button')
+    const widt = 10
+
+    //console.log(squeres)
+
+})
+
